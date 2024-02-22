17d0d032d7629da844304efa5d53d2b444bb1b95
Elyan <ekenimohelyanpheta@gmail.com>
2024-02-22 03:45:17 -0800

    Add:install.sh script

--- mine-genesis.sh
+++ mine-genesis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ADDR=${ADDR:-$(bitcoin-cli getnewaddress)}
-NBITS=${NBITS:-"1e0377ae"} #minimum difficulty in signet
+NBITS=${NBITS:-"1e0377ae"} #minimum difficulty in signet=this can be adjusted to reasonable time.
 miner --cli="bitcoin-cli" generate --address=$ADDR --grind-cmd="bitcoin-util grind" --nbits=$NBITS --set-block-time=$(date +%s)
